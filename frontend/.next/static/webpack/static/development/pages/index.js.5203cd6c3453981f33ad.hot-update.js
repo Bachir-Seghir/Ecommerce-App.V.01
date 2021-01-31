4e5a308c0abb71429b543491e299299d9cccd556
Bachir Seghir <bachir.seghir17@gmail.com>
2021-01-31 14:20:21 +0100

    initial commit with sign user added in backend

---
+++ frontend/.next/static/webpack/static/development/pages/index.js.5203cd6c3453981f33ad.hot-update.js
@@ -0,0 +1,40 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./components/Pagination.js":
+/*!**********************************!*\
+  !*** ./components/Pagination.js ***!
+  \**********************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "./node_modules/react/index.js");
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var _styles_PaginationStyles__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./styles/PaginationStyles */ "./components/styles/PaginationStyles.js");
+var _jsxFileName = "/Users/bachir/EcommerceMento/frontend/components/Pagination.js";
+
+
+
+var Pagination = function Pagination(props) {
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement(_styles_PaginationStyles__WEBPACK_IMPORTED_MODULE_1__["default"], {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 6
+    },
+    __self: this
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("p", {
+    __source: {
+      fileName: _jsxFileName,
+      lineNumber: 7
+    },
+    __self: this
+  }, "Hi I am the pagination"));
+};
+
+/* harmony default export */ __webpack_exports__["default"] = (Pagination);
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.5203cd6c3453981f33ad.hot-update.js.map
---
+++ frontend/pages/sell.js
@@ -0,0 +1,9 @@
+import CreateItem from '../components/CreateItem';
+
+const sell = (props) => (
+  <div>
+    <CreateItem />
+  </div>
+);
+
+export default sell;
