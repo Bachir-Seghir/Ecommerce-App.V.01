1886cc8f9035d7745a14341f8d68a7b5486084f9
Bachir Seghir <bachir.seghir17@gmail.com>
2021-01-31 21:13:51 +0100

    signup page and component created and the operation of creating a user in the db is successfull

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "46f82bd649675e1bbe0a";
+/******/ 	var hotCurrentHash = "455ab29558cbbcd5c1dd";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
