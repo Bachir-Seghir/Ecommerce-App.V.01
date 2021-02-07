81fb67dba553fd77f6029a184cdfa9606122b273
Bachir Seghir <bachir.seghir17@gmail.com>
2021-02-07 22:54:41 +0100

    permission checkbox updated login just on the frontend

--- frontend/.next/static/runtime/webpack.js
+++ frontend/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "21f936feee70facd8ff6";
+/******/ 	var hotCurrentHash = "217587b449d80634a863";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
